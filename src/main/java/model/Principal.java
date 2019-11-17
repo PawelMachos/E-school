6a96a41466cb250a5a03beea4a3f83d858681f74
Tomasz Żurawski <mgrtomaszzurawski@gmail.com>
2019-11-17 15:12:58 +0100

    updatedprincipal

--- src/main/java/model/Principal.java
+++ src/main/java/model/Principal.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Principal {
+import javax.persistence.*;
+
+
+@Entity(name = "Principa")
+@Table(name = "principat")
+public class Principal extends EntityBase {
+
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 }
+
